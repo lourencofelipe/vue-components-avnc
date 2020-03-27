99c77cb02326f054222cfd136046a664879e8944
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-27 14:55:29 -0300

    Creating component and passing data via props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
-    <h1>Vue Components</h1>
+    <h1>Vue JS</h1>
+
+    <Post title="Vue Components" />
+    <Post title="passing data via props" />
   </div>
 </template>
+
+<script>
+
+import Post from './components/Post.vue'
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
