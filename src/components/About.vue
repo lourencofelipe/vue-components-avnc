37aef2dae5605a6ace13d50fe91d58e04ba989cc
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-30 09:53:32 -0300

    Dynamic components

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,18 @@
 <template>
-    <h2>About</h2>
+    <div>
+        <h2>About</h2>
+
+        <p v-if="author">Author: {{ author }}</p>
+        <input type="text" v-model="author">
+    </div>
 </template>
+<script>
+
+export default {
+    data(){
+        return {
+            author: ''
+        }
+    },
+}
+</script>
